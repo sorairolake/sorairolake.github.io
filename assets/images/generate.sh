e75c249af567df32581275e74d98837bdfe1763f
Shun Sakai <sorairolake@protonmail.ch>
2024-09-10 23:15:47 +0900

    chore: Update profile image

--- assets/images/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-# SPDX-FileCopyrightText: 2023 Shun Sakai
-#
-# SPDX-License-Identifier: CC0-1.0
-
-resvg --background SkyBlue source.svg -c | oxipng -o max --out profile.png - && zopflipng -my profile.png profile.png
